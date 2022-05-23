6661458152ac29e8f3388d303d754c3fe6a0a363
Manuel García Cremades <mgarcia.severodam@hotmail.com>
2022-05-23 17:55:35 +0200

    Javadoc sumativo generado

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -1,7 +1,15 @@
 package sumador;
-
+/** Clase para sumar los valores numericos por
+ * los que esta formado un numero.
+ * 
+ * @author Manuel García Cremades
+ */
 public class ASumar {
-
+	/** 
+	* Suma individualmente los valores que componen un numero. 
+	* @param numero valor numerico cualquiera a sumar. 
+	* @return cadena del sumatorio y resultado.
+	*/ 
 	public String sumativo(String numero) {
 		int tamanyo = numero.length();
 		String resultado = "";
